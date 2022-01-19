9ad40c952a5d396a230884978f90d42490d5ecfb
dovanaite <d.dvarionaite@gmail.com>
2022-01-19 19:07:59 +0200

    eShop - done

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+const IsValid = require("./lib/IsValid.js");
 const jsonParse = require("./lib/jsonParse.js");
 const printList = require("./lib/printList.js");
 const readFile = require("./lib/readFile.js");
+const futureProfit = require("./lib/futureProfit.js");
+const inStockTotalCount = require("./lib/inStockTotalCount.js");
+const soldTotalCount = require("./lib/soldTotalCount.js");
+const profit = require("./lib/profit.js");
+const maxProfit = require("./lib/maxProfit.js");
 
 (async() => {
     const goods = [
-        // 'arbata',
-        // 'arba',
-        // 'kvepalai',
-        // 'masina',
+        'arbata',
+        'arba',
+        'kvepalai',
+        'masina',
         'masina-wrong-1',
         'masina-wrong-2',
         'masina-wrong-3',
@@ -29,16 +35,22 @@ const readFile = require("./lib/readFile.js");
         'masina-wrong-19',
         'masina-wrong-20',
         'masina-wrong-21',
-        // 'masina-wrong-22',
-        // 'masina-wrong-23',
-        // 'masina-wrong-24',
-        // 'masina-wrong-25',
-        // 'masina-wrong-26',
-        // 'masina-wrong-27',
-        // 'masina-wrong-28',
-        // 'masina-wrong-29',
-        // 'masina-wrong-30',
+        'masina-wrong-22',
+        'masina-wrong-23',
+        'masina-wrong-24',
+        'masina-wrong-25',
+        'masina-wrong-26',
+        'masina-wrong-27',
+        'masina-wrong-28',
+        'masina-wrong-29',
+        'masina-wrong-30',
         'pomidoras',
+        'masina-wrong-31',
+        'masina-wrong-32',
+        'masina-wrong-33',
+        'masina-wrong-34',
+        'masina-wrong-35',
+        'masina-wrong-36',
         '',
         5,
         true,
@@ -47,9 +59,11 @@ const readFile = require("./lib/readFile.js");
         () => {},
         [],
         {},
-        undefined
+        undefined,
+        'masina-wrong-37',
     ];
 
+    const availableCurrency = ['Eur', 'Usd', 'Lit'];
     const goodsInfo = [];
 
     for (const item of goods) {
@@ -64,11 +78,35 @@ const readFile = require("./lib/readFile.js");
         if (itemObj === false) {
             continue;
         }
+        const { name, price, inStock, sold } = itemObj;
+        if (!IsValid.correctObject(itemObj, 4) ||
+            !IsValid.nonEmptyString(name) ||
+            !IsValid.correctObject(price, 2) ||
+            !IsValid.nonNegativeNumber(price.value) ||
+            !IsValid.nonEmptyString(price.currency) ||
+            !availableCurrency.includes(price.currency) ||
+            !IsValid.nonNegativeInteger(inStock) ||
+            !IsValid.nonNegativeInteger(sold)) {
+            continue;
+        }
         goodsInfo.push(itemObj);
+        // console.log('ITEMS HERE', itemObj);
+        // console.log('ITEMS TEXT HERE', itemText);
     }
-
+    console.log('**********************************');
+    console.log('GOODS INFO', goodsInfo);
     console.log('"Univermagas" pardavime turi:');
     console.log('-----------------------------');
     console.log(printList(goodsInfo));
     console.log('-----------------------------');
+    console.log('Parduotuves suvestine:');
+    console.log(`- turimu prekiu sandelyje: ${inStockTotalCount(goodsInfo)}`);
+
+    console.log(`- parduotu prekiu: ${soldTotalCount(goodsInfo)}`);
+    console.log(`- suprekiauta suma: ${profit(goodsInfo)} ${goodsInfo[2].price.currency}`);
+    console.log(`- galimu pardavimu: ${futureProfit(goodsInfo)} ${goodsInfo[2].price.currency}`);
+    console.log(`- maksimali galima parduotuves apyvarta: ${maxProfit(goodsInfo)} ${goodsInfo[2].price.currency}`);
+
+    console.log('************************************')
+
 })();
